0724dac64114bba459765da59c7389d8c9804b59
Yota Toyama <raviqqe@gmail.com>
2016-10-27 10:37:10 +0900

    Add prototype Handler

---
+++ handler.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "net/http"
+)
+
+type Handler struct {}
+
+func NewHandler() Handler {
+  return Handler {}
+}
+
+func (Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+}
--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+  "net/http"
+  "log"
+)
+
 
 
 func main() {
+  s := newServer(":80");
+
+  log.Fatal(s.ListenAndServe())
+}
+
+
+func newServer(a string) http.Server {
+  return http.Server {
+    Addr: a,
+    Handler: NewHandler(),
+    ReadTimeout: 50,
+    WriteTimeout: 50,
+  }
 }
