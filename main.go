c5a466766bb69b18a094b86fe0556a25c24acc32
Yota Toyama <raviqqe@gmail.com>
2016-10-27 11:46:27 +0900

    Get command line arguments

--- main.go
+++ main.go
@@ -4,12 +4,31 @@ import (
   "log"
   "net/http"
   "time"
+
+  "github.com/docopt/docopt-go"
 )
 
 
 
 func main() {
-  serve(":80")
+  args := get_args()
+  a, _ := args["<address>"].(string)
+  serve(a)
+}
+
+
+func get_args() map[string]interface{} {
+  usage := `Usage:
+  metablog [-a <address>]
+
+Options:
+  -h --help     Show this help
+  -a --address  Set an listen address [default: 0.0.0.0:80]
+  --version     Show version
+`
+
+  args, _  := docopt.Parse(usage, nil, true, "metablog 0.0.0", false)
+  return args
 }
 
 
